db74e7f8583dffee3a515822bb209ce4105ade2f
hiki349 <vladyakima@gmail.com>
2024-05-24 14:27:42 +0300

    create pagination

--- client/src/components/ProjectList.vue
+++ client/src/components/ProjectList.vue
@@ -1,15 +1,24 @@
 <script setup>
-import ProjectItem from './ProjectItem.vue';
+import { ref } from 'vue'
+import ProjectItem from './ProjectItem.vue'
+import ProjectPagination from './ProjectPagination.vue'
 
-// TODO: create pagination
-defineProps({
-  data: [
-    {
-      id: String,
-      title: String
-    }
-  ]
-})
+// TODO: delete mock data
+const projects = Array.from({ length: 50 }, (_, i) => ({
+  id: `TGS-${i}`,
+  title: `Project ${10 + i}`
+}))
+
+const data = ref(projects.slice(0, 20))
+const activePage = ref(1)
+
+const changePage = (newPage) => {
+  const firstProject = (newPage - 1) * 20
+  const lastProject = firstProject + 20
+
+  data.value = projects.slice(firstProject, lastProject)
+  activePage.value = newPage
+}
 </script>
 
 <template>
@@ -17,5 +26,9 @@ defineProps({
     <div v-for="item in data" v-bind:key="item.id">
       <ProjectItem :id="item.id" :title="item.title" />
     </div>
+  </div>
+
+  <div class="flex justify-end pt-4">
+    <ProjectPagination @on-select="changePage" :count="5" :active="activePage" />
   </div>
 </template>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,14 +1,8 @@
 <script setup>
 import ProjectList from '@/components/ProjectList.vue'
-
-// TODO: delete mock data
-const projects = Array.from({ length: 20 }, (_, i) => ({
-  id: `TGS-${i}`,
-  title: `Project ${10 + i}`
-}))
 </script>
 
 <template>
   <h1 class="text-2xl mb-6 uppercase">Projects</h1>
-  <ProjectList :data="projects" />
+  <ProjectList />
 </template>
