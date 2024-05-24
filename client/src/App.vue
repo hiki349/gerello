4d122fa3fb05510dc843f2497db14f661d8aa845
hiki349 <vladyakima@gmail.com>
2024-05-24 13:08:23 +0300

    moved content from app to home view

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,9 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import ProjectList from './components/ProjectList.vue'
-
-// TODO: delete mock data
-const projects = Array.from({ length: 20 }, (_, i) => ({
-  id: `TGS-${i}`,
-  title: `Project ${10 + i}`
-}))
 </script>
 
 <template>
   <div class="max-w-7xl m-auto p-8">
-    <ProjectList :data="projects" />
-
     <RouterView />
   </div>
 </template>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,7 +1,14 @@
 <script setup>
+import ProjectList from '@/components/ProjectList.vue'
+
+// TODO: delete mock data
+const projects = Array.from({ length: 20 }, (_, i) => ({
+  id: `TGS-${i}`,
+  title: `Project ${10 + i}`
+}))
 </script>
 
 <template>
-  <main>
-  </main>
+  <h1 class="text-2xl mb-6 uppercase">Projects</h1>
+  <ProjectList :data="projects" />
 </template>
