5efacfa358ca5afa8f736635c76b4e2f4f71e1c7
hiki349 <vladyakima@gmail.com>
2024-05-26 00:18:21 +0300

    refactor

--- server/internal/adapters/postgres/projects/repo.go
+++ server/internal/adapters/postgres/projects/repo.go
@@ -1,1 +1,40 @@
 package projects
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	"gerello/internal/core/domain/project"
+)
+
+type ProjectRepositoryInPostgres struct {
+	db *pgxpool.Pool
+}
+
+func New(db *pgxpool.Pool) *ProjectRepositoryInPostgres {
+	return &ProjectRepositoryInPostgres{
+		db: db,
+	}
+}
+
+func (r *ProjectRepositoryInPostgres) Add(ctx context.Context, input *project.ProjectInput) (uuid.UUID, error) {
+	return uuid.UUID{}, nil
+}
+
+func (r *ProjectRepositoryInPostgres) Delete(ctx context.Context, id uuid.UUID) (bool, error) {
+	return false, nil
+}
+
+func (r *ProjectRepositoryInPostgres) Update(ctx context.Context, input *project.ProjectInput) (uuid.UUID, error) {
+	return uuid.UUID{}, nil
+}
+
+func (r *ProjectRepositoryInPostgres) GetAll(ctx context.Context) ([]project.Project, error) {
+	return nil, nil
+}
+
+func (r *ProjectRepositoryInPostgres) GetByID(ctx context.Context, id uuid.UUID) (*project.Project, error) {
+	return nil, nil
+}
--- server/internal/core/services/projects.go
+++ server/internal/core/services/projects.go
@@ -1,15 +1,42 @@
 package services
 
-import "gerello/internal/ports/db"
+import (
+	"context"
+
+	"github.com/google/uuid"
+
+	"gerello/internal/core/domain/project"
+	"gerello/internal/ports/db"
+)
 
 // TODO: Implement methods from ProjectService interface
 
-type ProjectServiceImpl struct {
+type ProjectService struct {
 	repo db.ProjectRepository
 }
 
-func New(repo db.ProjectRepository) *ProjectServiceImpl {
-	return &ProjectServiceImpl{
+func New(repo db.ProjectRepository) *ProjectService {
+	return &ProjectService{
 		repo: repo,
 	}
+}
+
+func (s *ProjectService) Add(ctx context.Context, input *project.ProjectInput) (*project.Project, error) {
+	return nil, nil
+}
+
+func (s *ProjectService) Delete(ctx context.Context, id uuid.UUID) (bool, error) {
+	return false, nil
+}
+
+func (s *ProjectService) Update(ctx context.Context, id uuid.UUID, input *project.ProjectInput) (*project.Project, error) {
+	return nil, nil
+}
+
+func (s *ProjectService) FetchAll(ctx context.Context) ([]project.Project, error) {
+	return nil, nil
+}
+
+func (s *ProjectService) FetchByID(ctx context.Context, id uuid.UUID) (*project.Project, error) {
+	return nil, nil
 }
